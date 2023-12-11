210c72c0b951a1de360be8a81bc89a40193703ea
ry4n <fossqueen@protonmail.com>
2023-12-11 05:50:54 +0000

    Cleaned up player script

--- player/player.gd
+++ player/player.gd
@@ -1,62 +1,55 @@
 extends RigidBody2D
 
-var ship
+var ship: Resource
 export var points: PoolVector2Array
-var buf_v
-var buf_v_length
-var camera
+var buf_v: Vector2
+var buf_v_length: float
+var camera: Node
 
-var reset_state = false
+var reset_state: bool = false
 var flight_assist: bool = true
 
-onready var rocket = load("res://_debug/rocket.tscn")
+const FUEL_EFFICIENCY: float = 0.0015
+const FLIGHT_ASSIST_AMOUNT: float = 0.6
 
-# base functions
-func _ready():
+func _ready() -> void:
 	set_variables()
 
-func _draw():
+
+func _draw() -> void:
 	draw_colored_polygon(ship.points, ship.color_black)
 	draw_polyline(ship.points, ship.color_white, ship.width, ship.antialiased)
 
-func _integrate_forces(_state):
+
+func _integrate_forces(_state) -> void:
 	movement()
 	store_velocity()
 	reset()
 
-func _physics_process(_delta):
+
+func _physics_process(_delta) -> void:
 	look()
 
+
 func _unhandled_input(event):
-	# System map
 	if Input.is_action_just_pressed("toggle_system_map"):
 		$UI.toggle_system_map()
 	
-	# Galaxy map
 	if Input.is_action_just_pressed("toggle_galaxy_map"):
-		print("ADD HERE toggle galaxy map")
-		#$UI.toggle_galaxy_map()
+		pass
 	
-	# Hyperspace
 	if Input.is_action_just_pressed("hyperspace"):
 		$UI/Radar.remove_all_objects()
 		reset_state = true
 		Global.main.hyperspace()
 		$UI/Radar.get_objects()
-
-	# Primary fire
+	
 	if event.is_action("primary_fire"):
 		$MiningLaser.is_casting = event.is_pressed()
 	
-	# Secondary fire
 	if Input.is_action_just_pressed("secondary_fire"):
-		var new_rocket = rocket.instance()
-		new_rocket.position = position
-		new_rocket.rotation = global_rotation
-		new_rocket.linear_velocity = linear_velocity
-		get_parent().add_child(new_rocket)
+		pass
 	
-	# Flight assist
 	if Input.is_action_just_pressed("flight_assist"):
 		toggle_flight_assist()
 	
@@ -64,73 +57,82 @@ func _unhandled_input(event):
 		$UI.toggle_ship_panel()
 	
 	if Input.is_action_just_pressed("ui_home"):
-		Global.save.player.inventory.add_item("wood", 1)
-		Global.save.player.inventory.add_item("poopball", 14)
-		Global.save.player.inventory.add_item("joint", 64)
+		Global.save.player.inventory.add_item("poopball", 1)
 
-# custom functions
-func movement():
+
+func movement() -> void:
 	if Input.is_action_pressed("thrust") and ship.fuel > 0:
 		apply_impulse(Vector2(0, 0).rotated(rotation), Vector2(ship.speed, 0).rotated(rotation))
 		$Animations/AnimationPlayer.play("thrust")
-		burn_fuel(0.0015)
+		burn_fuel(FUEL_EFFICIENCY)
 		linear_damp =  -1
 	else:
 		$Animations/AnimationPlayer.play("idle")
 		if flight_assist:
-			linear_damp = 0.6
+			linear_damp = FLIGHT_ASSIST_AMOUNT
 
-func look():
+
+func look() -> void:
 	look_at(get_global_mouse_position())
 
-func toggle_flight_assist():
+
+func toggle_flight_assist() -> void:
 	if flight_assist:
 		flight_assist = false
 	elif !flight_assist:
 		flight_assist = true
 
-func burn_fuel(amount):
+
+func burn_fuel(amount: float) -> void:
 	if ship.fuel > 0:
 		ship.fuel -= amount
 
-func store_velocity():
+
+func store_velocity() -> void:
 	buf_v = linear_velocity
 	buf_v_length = buf_v.length()
 
-func damage(amount):
+
+func damage(amount: float) -> void:
 	ship.health -= (amount / 10)
 	if ship.health <= 0:
 		destroy()
 
-func destroy():
+
+func destroy() -> void:
 	print("Ship destroyed")
 	queue_free()
 
-func reset():
+
+func reset() -> void:
 	if reset_state:
 		set_deferred("mode", MODE_KINEMATIC)
 		position = Vector2(0, 0)
 		set_deferred("mode", MODE_RIGID)
 		reset_state = false
 
-func set_variables():
+
+func set_variables() -> void:
 	Global.player = self
 	$UI/Radar.player = self
 	$UI/PlayerStats.player = self
 	camera = $Camera
 	Global.ui = $UI
 
-# signals
+
+# Signals
 func _on_Player_body_entered(body):
 	print("Player: Collision detected with %s" % body)
 	body.damage(buf_v_length)
 	damage(buf_v_length)
+
 
 func _on_Scanner_body_entered(body):
 	if body.is_in_group("radar_objects"):
 		$UI/Radar.add_object(body)
 	if body.is_in_group("npc"):
 		$UI/Radar.add_object(body)
+
 
 func _on_Scanner_body_exited(body):
 	if body.is_in_group("radar_objects"):
