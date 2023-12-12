bc4897adea934f5c0c95c48b808cf231b309561c
ry4n <fossqueen@protonmail.com>
2023-12-12 07:52:10 +0000

    Created ItemDatabase directory

---
+++ item/item-database.gd
@@ -0,0 +1,34 @@
+extends Resource
+class_name ItemDatabase
+
+const METALS: Dictionary = {
+	
+	"Gold": {
+		"Galactic Average": 100,
+	},
+	
+	"Silver": {
+		"Galactic Average": 75,
+	},
+	
+	"Platinum": {
+		"Galactic Average": 150,
+	},
+	
+	"Iron": {
+		"Galactic Average": 25,
+	},
+	
+	"Nickel": {
+		"Galactic Average": 25
+	},
+	
+	"Carbon": {
+		"Galactic Average": 5,
+	},
+	
+	"Phosphorus": {
+		"Galactic Average": 5,
+	},
+	
+}
