ec570817cd0c4eb556eb0c20c6fc72cec8d1877c
ry4n <fossqueen@protonmail.com>
2023-12-17 18:33:14 +0000

    Started Item Database

--- item/item-database.gd
+++ item/item-database.gd
@@ -1,34 +1,73 @@
-extends Resource
-class_name ItemDatabase
+extends Node
 
-const METALS: Dictionary = {
+
+enum ITEM_TYPE { CONSUMER, FOOD, ILLICIT, INDUSTRIAL, MEDICINE, MINERAL, SPECIALTY }
+
+
+const FUEL: Dictionary = {
 	
-	"Gold": {
-		"Galactic Average": 100,
+	"Cost": 10
+}
+
+
+const DATABASE: Dictionary = {
+	
+	0 : {
+		"Name": "Platinum",
+		"Type": ITEM_TYPE.MINERAL,
+		"Desc": "",
+		"Cost": 200,
 	},
 	
-	"Silver": {
-		"Galactic Average": 75,
+	1 : {
+		"Name": "Gold",
+		"Type": ITEM_TYPE.MINERAL,
+		"Desc": "",
+		"Cost": 100,
 	},
 	
-	"Platinum": {
-		"Galactic Average": 150,
+	2 : {
+		"Name": "Silver",
+		"Type": ITEM_TYPE.MINERAL,
+		"Desc": "",
+		"Cost": 100,
 	},
 	
-	"Iron": {
-		"Galactic Average": 25,
+	3 : {
+		"Name": "Iron",
+		"Type": ITEM_TYPE.MINERAL,
+		"Desc": "",
+		"Cost": 50,
 	},
 	
-	"Nickel": {
-		"Galactic Average": 25
+	4 : {
+		"Name": "Nickel",
+		"Type": ITEM_TYPE.MINERAL,
+		"Desc": "",
+		"Cost": 50,
 	},
 	
-	"Carbon": {
-		"Galactic Average": 5,
+	5 : {
+		"Name": "Diamonds",
+		"Type": ITEM_TYPE.MINERAL,
+		"Desc": "",
+		"Cost": 500,
 	},
 	
-	"Phosphorus": {
-		"Galactic Average": 5,
+	6 : {
+		"Name": "Welding Kit",
+		"Type": ITEM_TYPE.SPECIALTY,
+		"Desc": "Make your own repairs to your ship! Require's 'Scrap Metal'.",
+		"Cost": 1000,
 	},
+	
+	7 : {
+		"Name": "Scrap Metal",
+		"Type": ITEM_TYPE.INDUSTRIAL,
+		"Desc": "Processed minerals. Used for maintaining stations and in a pinch, ships!",
+		"Cost": 25,
+	},
+	
+	
 	
 }
